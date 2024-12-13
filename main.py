5b69f03abf974e7f98230a11245d99aa997c092e
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-12-13 21:02:57 +0100

    create logic to count each letter in text

--- main.py
+++ main.py
@@ -7,5 +7,18 @@ def main():
         total_num = len(remove_white_space)
         print(total_num)
 
-       
+        #count show many times a letters there are in the text ex: {'p': 6121}
+
+        words_dict = {}
+
+        for word in remove_white_space:
+            for char in word:
+                if char.isalpha():
+                    char = char.lower()
+                    if char in words_dict:
+                        words_dict[char] += 1
+                    else:
+                        words_dict[char] = 1
+
+        print(words_dict)
 main()
